be323b7bc04af41184023c20b5d00e0f6b1827d2
TimothyStiles <TimothySStiles@gmail.com>
2016-08-31 11:21:05 -0400

    initial commit. In repl produces sound from image sample.

---
+++ README.md
@@ -0,0 +1,32 @@
+# growasound
+
+Hey igem peoples and others. This is just a neat little proof of concept that turns an image into a sound.
+
+
+## Usage
+
+<code>cd</code> into the project and then execute <code>lein run</code> and you
+should hear a sound!
+
+If you haven't installed leiningen instructions can be found [here](leiningen.org).
+
+The first time you run the code you should run <code>lein deps</code> just to 
+make sure that your system has all of the depencies.
+
+On your first run it may be a little slow! No worries [Overtone](http://overtone.github.io/) is just installing.
+
+
+## license
+Copyright 2016 Timothy S. Stiles
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
---
+++ src/growasound/core.clj
@@ -0,0 +1,44 @@
+(ns growasound.core
+  (:use [funimage imp])
+  (:require 
+   [overtone.live :as o]
+   [overtone.inst.piano :as p])
+  (:gen-class))
+
+;; Introduction
+; Hey igem peoples and others. This is just a neat little proof of concept that turns an image into a sound.
+; "test-image" is a 96 X 96 image of a pokemon that I mutated with another project.
+; "get-pixels is a simple function that maps from the image plus data structure to compressed rgb values."
+; "quick song" takes test-image as a compressed RGB list and maps then to notes and rhythm and then plays the song."
+; To play the test just run lein repl inside the growasound direcotry and listen!
+;
+; -Tim
+
+(def test-image
+  (open-imp "resources/poke.png"))
+
+(defn get-pixels
+  "Takes an image and creates numerical values corresponding to pixels."
+  [imp]
+  (let [width (range 0 (get-width imp))
+        height (range 0 (get-height imp))]
+    (for [h height w width]
+      (get-pixel imp h w))))
+
+(defn quick-sound
+  "Takes values from get-pixels and makes a little song."
+  [pixels]
+  (let [filtered-pix (remove #(= % (first pixels)) pixels)
+        pos-pix (map #(if (pos? %) % (unchecked-negate %)) filtered-pix)
+        notes (map #(rem % 48) pos-pix)
+        rhythm (reverse (map #(rem % 500) pos-pix))]
+    (map #(o/at (+ (o/now) %1) (p/piano %2)) rhythm notes)))
+
+(defn -main 
+  "For playing out in BASH"
+  []
+  (quick-sound (get-pixels test-image)))
+
+
+
+     
