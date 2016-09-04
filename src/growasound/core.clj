24c69657e27d4ee0f5a920547b481deb1bca8456
TimothyStiles <TimothySStiles@gmail.com>
2016-09-04 18:57:19 -0400

    added sound recording.

--- src/growasound/core.clj
+++ src/growasound/core.clj
@@ -2,14 +2,22 @@
   (:use [funimage imp])
   (:require 
    [overtone.live :as o]
-   [overtone.inst.piano :as p])
+   [overtone.inst.piano :as p]
+   [overtone.studio.mixer :as m])
   (:gen-class))
 
 ;; Introduction
+
 ; Hey igem peoples and others. This is just a neat little proof of concept that turns an image into a sound.
+
 ; "test-image" is a 96 X 96 image of a pokemon that I mutated with another project.
-; "get-pixels is a simple function that maps from the image plus data structure to compressed rgb values."
-; "quick song" takes test-image as a compressed RGB list and maps then to notes and rhythm and then plays the song."
+
+; "get-pixels" is a simple function that maps from the image plus data structure to compressed 
+; rgb values.
+
+; "quick song" takes test-image as a compressed RGB list and maps then to notes and rhythm and 
+; then plays the song.
+
 ; To play the test just run lein repl inside the growasound direcotry and listen!
 ;
 ; -Tim
@@ -26,19 +34,19 @@
       (get-pixel imp h w))))
 
 (defn quick-sound
-  "Takes values from get-pixels and makes a little song."
+  "Takes values from get-pixels and makes a little sound."
   [pixels]
   (let [filtered-pix (remove #(= % (first pixels)) pixels)
         pos-pix (map #(if (pos? %) % (unchecked-negate %)) filtered-pix)
         notes (map #(rem % 48) pos-pix)
         rhythm (reverse (map #(rem % 500) pos-pix))]
-    (map #(o/at (+ (o/now) %1) (p/piano %2)) rhythm notes)))
+    (do (show-imp (copy-imp test-image))
+        (m/recording-start "poke-scream.wav")
+        (doall (map #(o/at (+ (o/now) %1) (p/piano %2)) rhythm notes))
+        (Thread/sleep 10000)
+        (m/recording-stop))))
 
-(defn -main 
+  (defn -main 
   "For playing out in BASH"
-  []
+  [&args]
   (quick-sound (get-pixels test-image)))
-
-
-
-     
